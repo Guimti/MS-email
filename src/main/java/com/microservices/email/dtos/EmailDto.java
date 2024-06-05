d908ca0661d5ca32410ac40afccb69b37cd5a0db
Guimti <guilhermeguimti@gmail.com>
2024-06-05 01:43:50 -0300

    Criação dos pacotes e classes

---
+++ src/main/java/com/microservices/email/dtos/EmailDto.java
@@ -0,0 +1,14 @@
+package com.microservices.email.dtos;
+
+
+import lombok.Data;
+
+@Data
+public class EmailDto {
+
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    private String text;
+}
